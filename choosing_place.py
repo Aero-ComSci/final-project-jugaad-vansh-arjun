e636e6c6c88b8b193073719dd1f4b1014f782dc6
Vansh624 <vansh.komarraju@gmail.com>
2025-05-27 10:34:14 -0700

    Update choosing_place.py
    Me and arjun did this

--- choosing_place.py
+++ choosing_place.py
@@ -1,6 +1,13 @@
-if location == "East":
+vac = input("Are you looking to go on a vaction this summer in the US?")
+if vac == "yes":
+    location = print("Would you like to go East or West?")
+        if location == "East":
             place_type == print("Would you like to be in a city or nature")
-                if place_type == "city":
+                if place_type == "city": 
                     print("Your top locations are ")
         elif location == "West"
             place_type == print("Would you like to be in a city or nature")
+
+elif vac == "no":
+    print("Come back when you are looking for a vacation!")
+else: print("Please say yes or no!")

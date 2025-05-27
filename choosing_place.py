19b6d00d4d4daea9bb5824bdc9cdfcb27b74b78f
Cooldudecode <arjun.mamidi777@gmail.com>
2025-05-27 10:32:32 -0700

    Update choosing_place.py

--- choosing_place.py
+++ choosing_place.py
@@ -1,1 +1,6 @@
-
+if location == "East":
+            place_type == print("Would you like to be in a city or nature")
+                if place_type == "city":
+                    print("Your top locations are ")
+        elif location == "West"
+            place_type == print("Would you like to be in a city or nature")

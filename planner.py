e7f0f2cc139c715cfd401abe65daf911bec591af
Jugaad30 <jugaad309@gmail.com>
2025-05-29 08:55:01 -0700

    Update planner.py

--- planner.py
+++ planner.py
@@ -4,3 +4,10 @@ num_days = ""
         if not num_days.isdigit() or int(num_days) <=0:
              print("Please enter a valid number")
    num_days = int(num_days)
+
+num_travellers = ""
+   while not num_travellers.isdigit() or int(num_travellers) <=0:
+        num_travellers = input ("How many people are travelling: ")
+        if not num_travellers.isdigit() or int(num_travellers) <=0:
+             print("Please enter a valid number")
+   num_travellers = int(num_travellers)

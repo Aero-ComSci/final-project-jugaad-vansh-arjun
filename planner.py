2e720a27636641ae8fc729566412686d3ea6b665
Jugaad30 <jugaad309@gmail.com>
2025-05-29 09:01:21 -0700

    Update planner.py

--- planner.py
+++ planner.py
@@ -11,3 +11,9 @@ num_travellers = ""
         if not num_travellers.isdigit() or int(num_travellers) <=0:
              print("Please enter a valid number")
    num_travellers = int(num_travellers)
+
+daily_plan = []
+   print("\nLets plan what you want to do each day!!!")
+   for day in range(1, num_days + 1):
+        activity = input(f"What do you want to do on Day {day}? (e.g Amusment Park, Eating, Fishing, Hiking, Sight-Seeing): ")
+        daily_plan.append(f"Day {day}: {activity}")

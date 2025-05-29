8b4cbcfc66f57767b888cecf3961f4d28e19dd4a
Cooldudecode <arjun.mamidi777@gmail.com>
2025-05-29 09:04:00 -0700

    Update choosing_place.py

--- choosing_place.py
+++ choosing_place.py
@@ -16,3 +16,18 @@ def get_destination_list(loc, loc_type):
                return west_city
              if (loc_type == "nature"):
                return west_nature
+def select_destination(destination_list):
+   print("\n!---Available Destinations---!")
+   i=0
+   while i < len(destination_selection_list):
+        print(f"{i+1}. {destination_selection_list[i]}")
+        i += 1
+   destination=""
+   size = len(destination_list)
+   while True:
+        choice = input("Enter the number of your favorite destination: ")
+        if choice.isdigit() and int(choice) > 0 and int(choice) <= size:
+             destination = destination_list[int(choice) - 1]
+             break
+        else: print("Please enter a valid input")
+   return destination

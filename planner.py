f1d7bf643654bad4f31f3a86097269c8ea884da3
Jugaad30 <jugaad309@gmail.com>
2025-05-29 08:45:50 -0700

    Update planner.py

--- planner.py
+++ planner.py
@@ -1,13 +1,6 @@
-destinations = ["Hawaii", "New York", "Yellowstone", "San Diego", "Los Angeles", "San Fransisco", "Lake Tahoe", "Florida", "Yosemite", "Alaska", "Las Vegas", "Boston", "Chicago"]
-print("Here are some top destinations for your summer vacation:")
-for place in destinations:
-    print("- " + place)
-
-activities = []
-for day in range(1,4):
-    activity = input("What would you like to do on Day {}?")
-    activities.append(activity)
-
-print("\nYoir Vacation Schedule:")
-for i in range(len(activities)):
-    print("Day {i + 1}: {activities[i]}")
+num_days = ""
+   while not num_days.isdigit() or int(num_days) <=0:
+        num_days = input ("How many days are you planning to travel: ")
+        if not num_days.isdigit() or int(num_days) <=0:
+             print("Please enter a valid number")
+   num_days = int(num_days)
